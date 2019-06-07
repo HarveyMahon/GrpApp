27ee5f6e1e51839c703c2a1481dd41642d5f68ff
ELsA & THoMaS (not marcus) <51322620+elsahahaha@users.noreply.github.com>
2019-06-07 12:14:05 +0100

    Update highScoring.java

--- Typing UI draft 1/src/sample/highScoring.java
+++ Typing UI draft 1/src/sample/highScoring.java
@@ -7,8 +7,12 @@ public class highScoring {
         int score = calcScore(charactersTyped, accuracy);
     }
     public int Scores(int score) {
-        if (score > highScore) highScore = score; //If the score is higher than the current then it replaces it.
-        return score;
-
+        if (score > highScore) {
+            highScore = score; //If the score is higher than the current then it replaces it.}
+            return score;
+        } else {
+            int close = highScore-score; //If they haven't achieved a new high score how close they were to the high score is returned
+            return close;
+        }
     }
 }
