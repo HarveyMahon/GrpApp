a81c66064e67d96d460a1fd6369308294d964212
ELsA & THoMaS (not marcus) <51322620+elsahahaha@users.noreply.github.com>
2019-06-07 12:28:26 +0100

    Added arraylist
    Done so that past scores are retrievable to be able to make a graph of past scores (desirable requirement)

--- Typing UI draft 1/src/sample/highScoring.java
+++ Typing UI draft 1/src/sample/highScoring.java
@@ -1,17 +1,23 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class highScoring {
     int highScore = -100; //Setting it to be lowest possible
+    List<Integer> copyOfScores = new ArrayList<>();
 
     public static void main(String[] args) {
         int score = calcScore(charactersTyped, accuracy);
     }
+
     public int Scores(int score) {
+        copyOfScores.add(score);
         if (score > highScore) {
             highScore = score; //If the score is higher than the current then it replaces it.}
             return score;
         } else {
-            int close = highScore-score; //If they haven't achieved a new high score how close they were to the high score is returned
+            int close = highScore - score; //If they haven't achieved a new high score how close they were to the high score is returned
             return close;
         }
     }
