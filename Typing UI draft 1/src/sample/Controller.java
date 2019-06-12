54d74459920ceb02a9fa5a25dc5c0bfb893261b3
ELsA & THoMaS (not marcus) <51322620+elsahahaha@users.noreply.github.com>
2019-06-12 13:10:33 +0100

    Text files updates

--- Typing UI draft 1/src/sample/Controller.java
+++ Typing UI draft 1/src/sample/Controller.java
@@ -1,4 +1,10 @@
 package sample;
 
+import javafx.scene.control.Label;
+
 public class Controller {
+    Label lbltextOutput;
+    private void thing() {
+        lbltextOutput.setText("hello world");
+    }
 }
