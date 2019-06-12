c61f839b067b33e615a6b25bf6eb57ec9591bd13
Marcus Connolly <ma.connolly@fettes.com>
2019-06-12 12:50:47 +0100

    selection liposuction

--- Typing UI draft 1/src/sample/Selection.java
+++ Typing UI draft 1/src/sample/Selection.java
@@ -28,201 +28,71 @@ public class Selection {
         
         Each level has 3 text files, which gets generated randomly 
         */}
+    Random rand = new Random();
+    int n = rand.nextInt(3);
+    //generates a random number between 0 and 2
+
+
     public void textSelection(){
         if (pressedHard) {
-            //generates a random number between 1 and 3
-            Random rand = new Random();
-            int n = rand.nextInt(3);
-            n += 1;
-
-            if (n==1) {
-                File file1 = new File("fileHard1.txt");
-                Scanner scanner = null;
-
-                try {
-                    scanner = new Scanner(file1);
-                } catch (FileNotFoundException e) {
-                    //handle this
-                }
-                //prints out the whole file
-                while (scanner.hasNextLine()) {         
-                    String line = scanner.nextLine();
-                    System.out.println(line);
-                    //return line;
-                }
-            }
-            else if (n==2){
-                File file2 = new File("fileHard2.txt");
-                Scanner scanner2 = null;
-
-                try {
-                    scanner2 = new Scanner(file2);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner2.hasNextLine()) {
-                    String line = scanner2.nextLine();
-                    System.out.println(line);
-                }
-            }
-            else if (n==3){
-                File file = new File("fileHard3.txt");
-                Scanner scanner3 = null;
-
-                try {
-                    scanner3 = new Scanner(file);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner3.hasNextLine()) {
-                    String line = scanner3.nextLine();
-                    System.out.println(line);
-                }
+            File file = null;
+            if (n == 0) {
+                file = new File("fileHard1.txt");
+            } else if (n == 1) {
+                file = new File("fileHard2.txt");
+            } else if (n == 2) {
+                file = new File("fileHard3.txt");
             }
         }
-        else if (pressedVHard) {          //Same is repeated for Very Hard
-            Random rand = new Random();
-            int n = rand.nextInt(3);
-            n += 1;
 
-            if (n==1) {
-                File file1 = new File("fileVHard1.txt");
-                Scanner scanner = null;
 
-                try {
-                    scanner = new Scanner(file1);
-                } catch (FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    System.out.println(line);
-                }
-            }
-            else if (n==2){
-                File file2 = new File("fileVHard2.txt");
-                Scanner scanner2 = null;
-
-                try {
-                    scanner2 = new Scanner(file2);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner2.hasNextLine()) {
-                    String line = scanner2.nextLine();
-                    System.out.println(line);
-                }
-            }
-            else if (n==3){
-                File file = new File("fileVHard3.txt");
-                Scanner scanner3 = null;
-
-                try {
-                    scanner3 = new Scanner(file);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner3.hasNextLine()) {
-                    String line = scanner3.nextLine();
-                    System.out.println(line);
-                }
+        else if (pressedVHard) {
+            File file = null;
+            if (n == 0) {
+                file = new File("fileVHard1.txt");
+            } else if (n == 1) {
+                file = new File("fileVHard2.txt");
+            } else if (n == 2) {
+                file = new File("fileVHard3.txt");
             }
         }
-        else if (pressedMedium) {          //Same is repeated for Medium
-            Random rand = new Random();
-            int n = rand.nextInt(3);
-            n += 1;
 
-            if (n==1) {
-                File file1 = new File("fileMedium1.txt");
-                Scanner scanner = null;
 
-                try {
-                    scanner = new Scanner(file1);
-                } catch (FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    System.out.println(line);
-                }
-            }
-            else if (n==2){
-                File file2 = new File("fileMedium2.txt");
-                Scanner scanner2 = null;
-
-                try {
-                    scanner2 = new Scanner(file2);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner2.hasNextLine()) {
-                    String line = scanner2.nextLine();
-                    System.out.println(line);
-                }
-            }
-            else if (n==3){              
-                File file = new File("fileMedium3.txt");
-                Scanner scanner3 = null;
-
-                try {
-                    scanner3 = new Scanner(file);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner3.hasNextLine()) {
-                    String line = scanner3.nextLine();
-                    System.out.println(line);
-                }
+        else if (pressedMedium) {
+            File file = null;
+            if (n == 0) {
+                file = new File("fileMedium1.txt");
+            } else if (n == 1) {
+                file = new File("fileMedium2.txt");
+            } else if (n == 2) {
+                file = new File("fileMedium3.txt");
             }
         }
-        else if (pressedEasy) {           //Same is repeated for Easy
-            Random rand = new Random();
-            int n = rand.nextInt(3);
-            n += 1;
 
-            if (n==1) {
-                File file1 = new File("fileEasy1.txt");
-                Scanner scanner = null;
 
-                try {
-                    scanner = new Scanner(file1);
-                } catch (FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    System.out.println(line);
-                }
+        else if (pressedEasy) {
+            File file = null;
+            if (n == 0) {
+                file = new File("fileEasy1.txt");
+            } else if (n == 1) {
+                file = new File("fileEasy2.txt");
+            } else if (n == 2) {
+                file = new File("fileEasy3.txt");
             }
-            else if (n==2){
-                File file2 = new File("fileEasy2.txt");
-                Scanner scanner2 = null;
+        }
 
-                try {
-                    scanner2 = new Scanner(file2);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner2.hasNextLine()) {
-                    String line = scanner2.nextLine();
-                    System.out.println(line);
-                }
-            }
-            else if (n==3){
-                File file = new File("fileEasy3.txt");
-                Scanner scanner3 = null;
 
-                try {
-                    scanner3 = new Scanner(file);
-                } catch(FileNotFoundException e) {
-                    //handle this
-                }
-                while (scanner3.hasNextLine()) {
-                    String line = scanner3.nextLine();
-                    System.out.println(line);
-                }
-            }
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            //handle this
+        }
+        //prints out the whole file
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+            //return line;
         }
     }
 }
